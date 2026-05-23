6ecb9ccf47dd9a85140d93a24644b54d4d75f2e8
UIT.23521444 <23521444@gm.uit.edu.vn>
2026-05-23 19:45:56 +0700

    Cập nhật

--- algorithms/correlation.py
+++ algorithms/correlation.py
@@ -3,9 +3,12 @@ def calculate_pearson(x, y):
         return 0, "Độ dài hai chuỗi không khớp."
     
     n = len(x)
-    x = [float(i) for i in x]
-    y = [float(i) for i in y]
     
+    try:
+        x = [float(i) for i in x]
+        y = [float(i) for i in y]
+    except ValueError:
+        return 0, "Dữ liệu chứa ký tự chữ"
     mean_x = sum(x) / n
     mean_y = sum(y) / n
     
@@ -16,9 +19,24 @@ def calculate_pearson(x, y):
         return 0, "Biến không đổi, không thể tính tương quan."
     
     r = num / den
-    
-    if abs(r) >= 0.7: text = "Tương quan rất chặt chẽ."
-    elif abs(r) >= 0.3: text = "Tương quan mức độ trung bình."
-    else: text = "Tương quan yếu hoặc không có."
-    
-    return round(r, 4), text
+    r_round = round(r, 4)
+    if r_round == 1:
+        text = "Tương quan thuận tuyệt đối."
+    elif 0.7 <= r_round < 1:
+        text = "Tương quan thuận rất mạnh."
+    elif 0.3 <= r_round < 0.7:
+        text = "Tương quan thuận ở mức trung bình."
+    elif 0 < r_round < 0.3:
+        text = "Tương quan thuận yếu."
+    elif r_round == 0:
+        text = "Không có tương quan tuyến tính."
+    elif -0.3 < r_round < 0:
+        text = "Tương quan nghịch yếu."
+    elif -0.7 <= r_round <= -0.3:
+        text = "Tương quan nghịch ở mức trung bình."
+    elif -1 < r_round <= -0.7:
+        text = "Tương quan nghịch rất mạnh."
+    else:
+        text = "Tương quan nghịch tuyệt đối."
+
+    return r_round, text
